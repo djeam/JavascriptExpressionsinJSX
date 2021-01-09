9ff9109a61af32403dd1279ae20a5b8f77005e79
djeam <60947080+djeam@users.noreply.github.com>
2021-01-09 08:18:53 -0500

    Practice with React using JSX Exercising the use of JavaScript expressions in a React file  through JSX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Gary";
+const lName = "Fleury";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}</h1>
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
